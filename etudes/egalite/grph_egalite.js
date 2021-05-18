952eaa54eebf210095f74105001e882a341626d9
netapy <39877685+netapy@users.noreply.github.com>
2021-05-18 22:23:35 +0200

    Update grph_egalite.js

--- etudes/egalite/grph_egalite.js
+++ etudes/egalite/grph_egalite.js
@@ -1,39 +1,3 @@
-Chart.types.Bar.extend({
-    name: "BarAlt",
-    initialize: function (data) {
-        Chart.types.Bar.prototype.initialize.apply(this, arguments);
-
-        if (this.options.curvature !== undefined && this.options.curvature <= 1) {
-            var rectangleDraw = this.datasets[0].bars[0].draw;
-            var self = this;
-            var radius = this.datasets[0].bars[0].width * this.options.curvature * 0.2;
-
-            // override the rectangle draw with ours
-            this.datasets.forEach(function (dataset) {
-                dataset.bars.forEach(function (bar) {
-                    bar.draw = function () {
-                        // draw the original bar a little down (so that our curve brings it to its original position)
-                        var y = bar.y;
-                        // the min is required so animation does not start from below the axes
-                        bar.y = Math.min(bar.y + radius, self.scale.endPoint - 1);
-                        // adjust the bar radius depending on how much of a curve we can draw
-                        var barRadius = (bar.y - y);
-                        rectangleDraw.apply(bar, arguments);
-
-                        // draw a rounded rectangle on top
-                        Chart.helpers.drawRoundedRectangle(self.chart.ctx, bar.x - bar.width / 2, bar.y - barRadius + 1, bar.width, bar.height, barRadius);
-                        ctx.fill();
-
-                        // restore the y value
-                        bar.y = y;
-                    }
-                })
-            })
-        }
-    }
-});
-
-
 var ctxx = document.querySelector('#chart1_sbf120_secteurs').getContext('2d');
 var chart = new Chart(ctxx, {
     type: 'horizontalBar',
@@ -88,7 +52,6 @@ var chart = new Chart(ctxx, {
             label: "Indice égalité",
             backgroundColor: '#6219D8',
             data: data_top_pot_sbf["data"].slice(0, 10).map(x => x[0]),
-            borderRadius:20,
         }]
     },
     options: {
